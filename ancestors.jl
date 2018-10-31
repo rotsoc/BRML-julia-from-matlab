98f0a9f169f05511c680dd1e97d66f1fa45b794a
rotsoc <44649432+rotsoc@users.noreply.github.com>
2018-10-31 19:31:18 +0200

    Add files via upload

---
+++ ancestors.jl
@@ -0,0 +1,29 @@
+function ancestors(x,A)
+    # ancestors of nodes x in DAG A
+    # e.g. ancestors([1 2],A)
+    N=size(A,1)
+    # include self transitions
+    for i=1:N,j=1:N
+        if i==j
+            A[i,j]+=1
+        end
+    end
+    # find descendants after N
+    # updates (excluding self-self paths)
+    AD=A^N
+    for i=1:N,j=1:N
+        AD[i,j]=Int(AD[i,j]>0)
+        if i==j
+            AD[i,j]-=1
+        end
+    end
+    # find ancestors
+    a=[]
+    for i=x
+        t = findall(y->y>0,vec(AD[:,i]))
+        t = t'
+        a=unique([a... t])
+    end
+    a=setdiff(a,x)
+    return a
+end
